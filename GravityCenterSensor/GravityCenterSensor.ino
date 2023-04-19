8772cd8f9fbe8e4f572cb2d676846ca65a717b00
wei_an <wei_an@phalanity.com.tw>
2023-04-19 11:16:16 +0800

    加入專案檔案。

---
+++ GravityCenterSensor/GravityCenterSensor.ino
@@ -0,0 +1,22 @@
+﻿/*
+ Name:		GravityCenterSensor.ino
+ Created:	2023/4/19 上午 11:15:41
+ Author:	wei_an
+*/
+
+//https://www.youtube.com/watch?v=_RUZtsQzSLY&list=PL_i0cTCK_y-j5rLACIJ1QtrNvCDZPHAZT&index=8
+
+/// <summary>
+/// 
+/// </summary>
+const int SerialPort = 9600;
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+
+}
